4203bfd6ffc0c2bbd85db3cb685c8416e480d502
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-13 16:01:11 +0200

    TS, return image

---
+++ netlify/functions/og-image.ts
@@ -0,0 +1,17 @@
+import fs from "fs/promises";
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  const img = await fs.readFile('./public/pic.png', {encoding: 'base64'});
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/png'
+    },
+    body: img.toString(),
+    isBase64Encoded: true
+  };
+};
+
+export { handler };
