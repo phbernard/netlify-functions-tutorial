1ba09dee9cf86047a6f12f64688ea49d44f27449
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-14 12:12:10 +0200

    Refactoring

--- netlify/functions/og-image.ts
+++ netlify/functions/og-image.ts
@@ -2,16 +2,38 @@ import { promises as fs } from "fs"
 
 import { Handler } from "@netlify/functions";
 
-import { compileLocalTemplate } from '@resoc/create-img'
-import { FacebookOpenGraph } from '@resoc/core'
+const chromium = require('chrome-aws-lambda');
+
+//import { compileLocalTemplate } from '@resoc/create-img'
+//import { FacebookOpenGraph } from '@resoc/core'
 
 const handler: Handler = async (event, context) => {
+  /*
   await compileLocalTemplate('./assets/resoc-template/resoc.manifest.json', {
     title: "Hello!!",
     mainImageUrl: "https://resoc.io/assets/img/demo/photos/pexels-photo-371589.jpeg",
     textColor: "#fff",
     backgroundColor: "#20552a"
   }, FacebookOpenGraph, './pic.png');
+  const img = await fs.readFile('./pic.png', {encoding: 'base64'});
+  */
+
+  const browser = await chromium.puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  // Wait until there are no network connexion for 500ms
+  await page.goto('http://realfavicongenerator.net', {waitUntil: [
+    'networkidle0', 'domcontentloaded', 'load'
+  ]});
+  const output = './pic.png';
+  await page.screenshot({
+    path: output,
+    fullPage: true
+  });
+
+  await browser.close();
+
   const img = await fs.readFile('./pic.png', {encoding: 'base64'});
 
   return {
