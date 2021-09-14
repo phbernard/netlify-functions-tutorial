d6d97828d41a0908498d79f0366353e22aa44d3d
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-14 10:32:06 +0200

    Attempt to build image

--- netlify/functions/og-image.ts
+++ netlify/functions/og-image.ts
@@ -2,8 +2,17 @@ import { promises as fs } from "fs"
 
 import { Handler } from "@netlify/functions";
 
+import { compileLocalTemplate } from '@resoc/create-img'
+import { FacebookOpenGraph } from '@resoc/core'
+
 const handler: Handler = async (event, context) => {
-  const img = await fs.readFile('./assets/pic.png', {encoding: 'base64'});
+  await compileLocalTemplate('./assets/resoc-template/resoc.manifest.json', {
+    title: "Hello!!",
+    mainImageUrl: "https://resoc.io/assets/img/demo/photos/pexels-photo-371589.jpeg",
+    textColor: "#fff",
+    backgroundColor: "#20552a"
+  }, FacebookOpenGraph, './pic.png');
+  const img = await fs.readFile('./pic.png', {encoding: 'base64'});
 
   return {
     statusCode: 200,
